3be33b5b01357c0e067c83376a84457cf78b3ac3
Ilie Ciotir <dipixelhunter@gmail.com>
2014-11-06 00:36:17 +0200

    write the abouts of the items

--- js/app.js
+++ js/app.js
@@ -6,11 +6,15 @@ angular.module('htmldojo').controller('Dojo', ['$scope', 'StoriesSvc', function(
 angular.module('htmldojo').factory('StoriesSvc', [function(){
 	var webSamurai = {
 			title: "Web Samurai",
-			description: "Say something about this",
+			description: {
+				ps: ['Set in the mythical Realm 101 the story sets the user in the place of a child joining the local CoderDojo in the hope of one day becomming a mighty Web Samurai.',
+				'The first chapter follows the pupil\'s first day in the dojo. The story quickly escalates when the dojo is attacked by an yet unkown entity.',
+				'The second chapter is still under development.']
+			},
 			author:"Ilie",
 			hallOfFame: {
 				the1st: {
-					name: "TBD",
+					name: "Muller Eduard",
 					avatar: 'images/avatars/malfurion.png'
 				},
 				mentions: []
